c73419110d316865fda2003ee8987125f02e253e
Steve Sanderson <steve@codeville.net>
2012-05-16 16:08:02 +0100

    Support custom filename extensions

--- ejs-middleware.js
+++ ejs-middleware.js
@@ -1,17 +1,19 @@
 var url = require('url'),
     fs = require('fs'),
     path = require('path'),
-    extension = '.ejs';
+    defaultExtension = 'ejs';
 
-module.exports = function(dir) {
+module.exports = function(dir, extension) {
     return function(req, res, next) {
         var pathname = path.join(dir, url.parse(req.url).pathname);
+        extension = extension || defaultExtension;
+
         fs.lstat(pathname, function(err, stats) {
             // Requests that match /dir will be interpreted as /dir/index
             if(!err && stats.isDirectory()) {
                 pathname = path.join(pathname, 'index');
             }
-            pathname += extension;
+            pathname += '.' + extension;
 
             fs.lstat(pathname, function(err, stats) {
                 if(!err && stats.isFile()) {
