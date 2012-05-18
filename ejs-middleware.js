75eca68ad87058b63160ed389398e8af6519bf1b
Steve Sanderson <steve@codeville.net>
2012-05-18 17:37:55 +0100

    Auto-register renderer

--- ejs-middleware.js
+++ ejs-middleware.js
@@ -1,9 +1,14 @@
 var url = require('url'),
     fs = require('fs'),
     path = require('path'),
+    ejs = require('ejs'),
     defaultExtension = 'ejs';
 
-module.exports = function(dir, extension) {
+module.exports = function(dir, extension, registerInApp) {
+    if (registerInApp) {
+        registerInApp.register('.' + extension, ejs);
+    }
+
     return function(req, res, next) {
         var pathname = path.join(dir, url.parse(req.url).pathname);
         extension = extension || defaultExtension;
